f5354f074f9a8bed18963e1fc0cb65bf2054f0ee
Kleponaeru <kelvinlie08@gmail.com>
2025-03-18 15:27:46 +0700

    react bits tilted card

--- client/src/components/TiltedCard.jsx
+++ client/src/components/TiltedCard.jsx
@@ -21,6 +21,7 @@ export default function TiltedCard({
   showTooltip = true,
   overlayContent = null,
   displayOverlayContent = false,
+  ...props
 }) {
   const ref = useRef(null);
   const x = useMotionValue(0);
@@ -74,7 +75,9 @@ export default function TiltedCard({
   return (
     <figure
       ref={ref}
-      className="relative w-full h-full [perspective:800px] flex flex-col items-center justify-center"
+      className={`relative w-full h-full [perspective:800px] flex flex-col items-center justify-center ${
+        props.className || ""
+      }`}
       style={{
         height: containerHeight,
         width: containerWidth,
